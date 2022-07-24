5425938181a1bf55d31bc0c8de10872010da78b8
SouthHealer628 <108107785+wspbh@users.noreply.github.com>
2022-07-24 13:57:57 +0800

    Update ub-i.sh

--- ub-i.sh
+++ ub-i.sh
@@ -3,10 +3,10 @@ echo -e "\033[34m 作者:wspbh github地址:www.github.com/wspbh
 确保你的网络正常，记得关流量XD否则安装失败不怪我                                                                                       qq:1769481479
 目前只支持ubuntu20.04.4LTS版本\033[0m"
 read -p '按回车以开始安装'
-read -n1 -p "你想将源改为bfsu国内源吗?(推荐更改)   [输Y以更改/任意键不更改]?" answer                                                   case $answer in
-Y | y )
-      sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.bfsu.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sou
-rces.list;;
+read -n1 -p "你想将源改为bfsu国内源吗?(推荐更改)   [输Y以更改/任意键不更改]?" answer
+case $answer in
+Y | y )                                               
+      sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.bfsu.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list;;
 *)
       echo '不换源';;
 esac
